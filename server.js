08638f067d41e0f8f4423ae679babb7453a55611
Johanna Reisswig <johannareisswig@gmail.com>
2019-11-11 16:50:38 +0100

    patch, delete, post

--- server.js
+++ server.js
@@ -5,10 +5,35 @@ let cards = require('./cards.json')
 cards.forEach(card => (card.id = uid()))
 
 const app = express()
+app.use(express.json())
 app.listen(3334, () => console.log('express ready on Port 3334'))
 
 app.get('/cards', (req, res) => {
   res.json(cards)
+})
+
+app.get('/cards/:id', (req, res) => {
+  const id = req.params.id
+  const card = cards.find(card => card.id === id)
+  res.json(card)
+})
+
+app.post('/cards', (req, res) => {
+  console.log(req.body)
+  const newCard = req.body
+  newCard.id = uid()
+  cards = [...cards, newCard]
+  res.json(cards)
+})
+
+app.patch('/cards/:id', (req, res) => {
+  const id = req.params.id
+  const index = cards.findIndex(card => card.id === id)
+
+  let cardToEdit = cards[index]
+  cardToEdit = { ...cards[index], ...req.body }
+  // console.log(cardToEdit)
+  res.json(cardToEdit)
 })
 
 app.delete('/cards/:id', (req, res) => {
