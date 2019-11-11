451bbdddb2087c23143d1f922304305499bd3793
Johanna Reisswig <johannareisswig@gmail.com>
2019-11-11 14:55:54 +0100

    Backend mit Express

--- server.js
+++ server.js
@@ -1,35 +1,20 @@
-const http = require('http')
+const express = require('express')
 const uid = require('uid')
 
 let cards = require('./cards.json')
 cards.forEach(card => (card.id = uid()))
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'DELETE') {
-    const id = req.url.replace('/cards/', '')
-    const index = cards.findIndex(card => card.id === id)
-    const cardToDelete = cards[index]
-    cards.splice(index, 1)
-    res.writeHead(200, {
-      'content-type': 'application/json; charset: utf-8'
-    })
+const app = express()
+app.listen(3334, () => console.log('express ready on Port 3334'))
 
-    res.end(JSON.stringify(cardToDelete))
-
-    console.log(req.url, req.method)
-    const prettyPath = req.url.slice(1)
-    // res.end(`<h1> Your request: ${prettyPath}</h1>`)
-
-    if (req.url === '/cards' && req.method === 'GET') {
-      res.writeHead(200, {
-        'content-type': 'application/json; charset: utf-8'
-      })
-      res.end(JSON.stringify(cards))
-    }
-
-    res.statusCode = 404
-    res.end('404 - nothing found')
-  }
+app.get('/cards', (req, res) => {
+  res.json(cards)
 })
 
-server.listen(3333)
+app.delete('/cards/:id', (req, res) => {
+  const id = req.params.id
+  const index = cards.findIndex(card => card.id === id)
+  const cardToDelete = cards[index]
+  cards.splice(index, 1)
+  res.json(cardToDelete)
+})
