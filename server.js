db160b917d108b31c66c5f5f29cc8510aef36830
Johanna Reisswig <johannareisswig@gmail.com>
2019-11-12 12:07:11 +0100

    Write files

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+const fs = require('fs')
 const express = require('express')
 const uid = require('uid')
 
 let cards = require('./cards.json')
-cards.forEach(card => (card.id = uid()))
-
+//cards.forEach(card => (card.id = uid()))
+function saveCards(cards, res, output) {
+  fs.writeFile('./cards.json', JSON.stringify(cards, null, 2), err => {
+    if (err) {
+      res.end('Error: Could not write file')
+    } else {
+      res.json(output)
+    }
+  })
+}
 const app = express()
 app.use(express.json())
 app.listen(3334, () => console.log('express ready on Port 3334'))
@@ -23,17 +32,18 @@ app.post('/cards', (req, res) => {
   const newCard = req.body
   newCard.id = uid()
   cards = [...cards, newCard]
-  res.json(cards)
+  res.json(newCard)
+  saveCards(cards, res, newCard)
 })
 
 app.patch('/cards/:id', (req, res) => {
   const id = req.params.id
   const index = cards.findIndex(card => card.id === id)
-
   let cardToEdit = cards[index]
   cardToEdit = { ...cards[index], ...req.body }
   // console.log(cardToEdit)
   res.json(cardToEdit)
+  saveCards(cards, res, cardToEdit)
 })
 
 app.delete('/cards/:id', (req, res) => {
@@ -42,4 +52,5 @@ app.delete('/cards/:id', (req, res) => {
   const cardToDelete = cards[index]
   cards.splice(index, 1)
   res.json(cardToDelete)
+  saveCards(cards, res, cardToDelete)
 })
